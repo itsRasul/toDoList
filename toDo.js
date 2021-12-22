cc45c86d40aed5a32c330f1f3cbab828261c4760
mohamadRasul mirzabeygi <its.rasulmirzabeygi@gmail.com>
2021-12-22 17:58:40 +0330

    edit item feature

--- toDo.js
+++ toDo.js
@@ -9,10 +9,49 @@ work.focus();
 
 // selectors
 const plusBtn = document.querySelector("#plus-button-todo");
+const closeFullScreenEdit = document.querySelector(".close-fullscreen");
+
+
 // functions
+
+function closeFullScreenEditFun(e){
+    let closeBtn = this;
+    let fullPage = closeBtn.parentNode;
+
+    fullPage.classList.replace("d-flex", "d-none")
+}
+
+function setEditInputToItsToDoItem(toDoItem , checkFullScreen){
+    return function() {
+        let input = checkFullScreen.previousElementSibling;
+        let value = input.value;
+
+        closeFullScreenEditFun.call(closeFullScreenEdit);
+        toDoItem.firstChild.textContent = value;
+    }
+}
+
+
+function editItem(e){
+    editIcon = this;
+    let toDoItem = editIcon.parentNode.parentNode;
+
+    let fullscreenEditPage = document.querySelector("#edit-input-fullscreen-page-container");
+    fullscreenEditPage.classList.replace("d-none", "d-flex");
+
+    let firstValue = toDoItem.firstChild.textContent;
+    
+    let inputFullScreen = fullscreenEditPage.querySelector("input[name='edit']")
+    inputFullScreen.value = firstValue;
+    inputFullScreen.focus();
+    const checkFullScreen = document.querySelector(".check-fullscreen");
+    checkFullScreen.addEventListener("click" , setEditInputToItsToDoItem(toDoItem , checkFullScreen));
+    
+}
+
 function deleteItem(e){
     let iconTimes = this;
-    let todoItem = iconTimes.parentNode;
+    let todoItem = iconTimes.parentNode.parentNode;
     let todoItemContainer = todoItem.parentNode;
 
     todoItemContainer.removeChild(todoItem);
@@ -24,17 +63,21 @@ function addItemToDoListProccess(e){
 
     let todoItem = document.createElement("div");
     todoItem.innerHTML = work.value;
-    todoItem.classList.add("todo-item", "bg-dark", "text-white", "h6", "text-center","p-2","pr-4", "rounded-lg")
+    todoItem.classList.add("todo-item", "bg-dark", "text-white", "h6", "text-center","p-2", "rounded-lg")
     
     todoItemContainer.appendChild(todoItem);
-
+    // iconTimes => icon-container
     let iconTimes = document.createElement("div");
     iconTimes.classList.add("todo-item-icons-container" , "float-right");
     iconTimes.style.cursor = "pointer";
-
+    // iIcon => deleteIcon(i)
     let iIcon = document.createElement("i");
     iIcon.classList.add("fas" , "fa-times" , "tofo-item-icon");
 
+    let editIcon = document.createElement("i");
+    editIcon.classList.add("far" , "fa-edit" , "mr-1" , "edit-icon");
+
+    iconTimes.appendChild(editIcon);
     iconTimes.appendChild(iIcon);
     todoItem.appendChild(iconTimes);
 
@@ -47,7 +90,10 @@ function addItemToDoListProccess(e){
     // clear the input
     work.value = "";
     work.focus();
-    iconTimes.addEventListener("click" , deleteItem);    
+
+    //Events about icons
+    iIcon.addEventListener("click" , deleteItem);
+    editIcon.addEventListener("click" , editItem); 
 }
 
 function addItemToDoListProccessKey(e){
@@ -65,3 +111,4 @@ function limitChar(e){
 plusBtn.addEventListener("click" , addItemToDoListProccess);
 work.addEventListener("keyup" , addItemToDoListProccessKey);
 work.addEventListener("keypress" , limitChar);
+closeFullScreenEdit.addEventListener("click" , closeFullScreenEditFun);
